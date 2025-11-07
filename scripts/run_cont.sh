bc684f5eb73be950d86c9335f2eb256b557a34f6
Vamsi Eyunni <vamsieyunni@gmail.com>
2025-11-07 12:12:25 -0800

    add autocommit to run script

--- scripts/run_cont.sh
+++ scripts/run_cont.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+
+AUTOCOMMIT="${1:-}"
+
 docker start -ai slow-pylot
+
+NEW_TAG="slow-pylot:$(date +%Y%m%d-%H%M)"
+MSG="auto-commit $(date -Is)"
+
+if [[ "$AUTOCOMMIT" == "autocommit" ]]; then
+  # Always commit after exit
+  docker commit \
+    --author "auto" \
+    --message "$MSG" \
+    "slow-pylot" "slow-pylot:dev"
+
+  echo "Committed container as image: $NEW_TAG"
+fi
