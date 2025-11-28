88c1b286f55c4242f708d738607a687b7a222833
Vamsi Eyunni <vamsieyunni@gmail.com>
2025-11-27 19:00:49 -0500

    RESTRUCTURE DIRECTORY

--- scripts/mininet/sync_mininet_files.sh
+++ scripts/mininet/sync_mininet_files.sh
@@ -5,7 +5,7 @@ REMOTE_USER="wifi"
 REMOTE_HOST="127.0.0.1"
 REMOTE_PORT="2222"
 
-REMOTE_PATH="/home/wifi/carla-project/"
+REMOTE_PATH="/home/wifi/M202A-CARLA/scripts/mininet/"
 LOCAL_PATH="$HOME/M202A-CARLA/scripts/mininet/"
 
 echo "password is wifi"
